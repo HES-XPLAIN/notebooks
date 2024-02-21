f98384ccb93badb24fc24ce892eff741d3e91995
arthurbabey <a.babey5@gmail.com>
2024-02-21 17:53:39 +0100

    use_case notebook stable

--- use_case_sport_classification/scripts/models.py
+++ use_case_sport_classification/scripts/models.py
@@ -3,9 +3,9 @@ import torch.nn as nn
 import torchvision.models as models
 
 class FineTunedVGG(nn.Module):
-    def __init__(self, num_classes=100, weights='IMAGENET1K_V1'):
+    def __init__(self, num_classes=100):
         super(FineTunedVGG, self).__init__()
-        vgg = models.vgg16(weights=weights)  # Load pre-trained VGG16 model
+        vgg = models.vgg16(weights=models.VGG16_Weights.IMAGENET1K_V1)  # Load pre-trained VGG16 model
 
         vgg.classifier[-1] = nn.Linear(4096, num_classes)  # Modify the last fully connected layer
         self.features = vgg.features
@@ -16,20 +16,17 @@ class FineTunedVGG(nn.Module):
         # Freeze all layers
         for param in vgg.parameters():
             param.requires_grad = False
-
-        for param in vgg.classifier[3].parameters():
-            param.requires_grad = True
-        for param in vgg.classifier[6].parameters():
-            param.requires_grad = True
         """
-
+        
     def forward(self, x):
         x = self.features(x)
         x = self.avgpool(x)
         x = torch.flatten(x, 1)
-        x = self.classifier(x) 
+        x = self.classifier(x)  # 3 or 6 (after relu dropout of fc1 or fc2)
 
         return x
+        
+        
 
 class FineTunedEfficientNet(nn.Module):
     def __init__(self, num_classes=100, weights='IMAGENET1K_V1'):
